7a59be215939fe7b4a5b80c4bff2de0dcc5b1652
pixsplanet <76448871+pixsplanet@users.noreply.github.com>
2023-01-10 11:41:25 -0700

    The very first version
    :D

---
+++ pixlib.py
@@ -0,0 +1,17 @@
+####            Made by pixsplanet            ####
+# This is just a QOL and fun library for Python. #
+
+import os
+import time
+
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def doNothing():
+    time.sleep(0)
+
+def PLcredits():
+    print('PixLib\nCreated by pixsplanet\nThe worst Python library ever\nEnjoy!')
+
+def pix():
+    print('-----------\n|         |\n|  |   |  |\n| -_____- |\n|         |\n-----------')
